725d569ab52b53ea918a79b91ebf83f43aa5ab42
Dmitriy Onypko <vaporif@gmail.com>
2024-07-16 17:20:42 +0100

    add repo

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+use cgp_core::component::HasComponents;
+
 fn main() {
     println!("Hello, world!");
 }
+
+pub struct Repository;
+
+impl HasComponents for Repository {
+    type Components = RepositoryComponents;
+}
+
+pub struct RepositoryComponents;
