946b30ffe2acfcf8aca8f0ca38bb4e8c1fbd996e
Dmitriy Onypko <vaporif@gmail.com>
2024-07-19 10:54:39 +0100

    rework

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,30 @@
-use std::u16;
-
 use cgp_core::prelude::*;
 
 fn main() {}
 
 pub struct RepositoryComponents;
-#[derive_component(ItemGetterComponent, ItemsGetter<Context>)]
-pub trait CanFormatItems {
-    fn has_item(&mut self, item: String) -> bool;
+#[derive_component(ItemCheckComponent, ItemChecker<Context>)]
+pub trait HasItem {
+    fn has_item(&self, item: String) -> bool;
 }
 
 pub struct GetItemFromMemory;
 
-impl<Context> ItemsGetter<Context> for GetItemFromMemory
+impl<Context> ItemChecker<Context> for GetItemFromMemory
 where
     Context: CanGetDb,
 {
-    fn has_item(context: &mut Context, item: String) -> bool {
+    fn has_item(context: &Context, item: String) -> bool {
         let db = context.get_db();
         db.contains(&item)
     }
 }
 
 delegate_components!(RepositoryComponents {
-    ItemGetterComponent: GetItemFromMemory,
+    ItemCheckComponent: GetItemFromMemory,
 });
 
 #[derive_component(DbGetterComponent, DbGetter<Context>)]
 pub trait CanGetDb {
-    fn get_db(&mut self) -> Vec<String>;
+    fn get_db(&self) -> Vec<String>;
 }
